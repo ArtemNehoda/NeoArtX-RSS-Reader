a9ad3b28a4feea2da2dd4945267d1f5e9f4fb503
Artem Nehoda <negoda1995@bk.ru>
2018-12-24 12:31:08 +0000

    fs

--- src/rssParser.js
+++ src/rssParser.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 
 export default (data) => {
   const parsedData = new DOMParser().parseFromString(data, 'application/xml');
-  if (parsedData.querySelector('rss').length <= 0) { throw new Error('is not RSS'); }
+  if (!parsedData.querySelector('rss')) { throw new Error('is not RSS'); }
   const channel = parsedData.querySelector('channel');
   const children = [...channel.querySelectorAll('item')].map(item => ({
     guid: _.uniqueId(),
